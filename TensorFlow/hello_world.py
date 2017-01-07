ef87016e8ebfcfa132a58b4c7c3664609af69ab6
Guruprasad Venkataraghavan <vguruprasad91@gmail.com>
2017-01-07 14:09:05 -0500

    wrote code for tensorflow hello world

--- TensorFlow/hello_world.py
+++ TensorFlow/hello_world.py
@@ -1,1 +1,12 @@
 
+# import tensorflow
+import tensorflow as tf
+
+# Create a tensorflow object called hello_constant
+hello_constant = tf.constant('Hello World!')
+
+# Create a tensorflow session
+with tf.Session() as sess:
+  # Run the tf.constant operation in the session
+  output = sess.run(hello_constant)
+  print(output)
